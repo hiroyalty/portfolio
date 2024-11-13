c20ce4b43efee4ffd5270580a1582871cb959116
Gbolaga Famodun <gbolaga.famodun@gmail.com>
2024-11-12 23:16:03 -0500

    Replaced old code with new code

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,57 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    rotate: {
+      "-180": "-180deg",
+      "-90": "-90deg",
+      "-45": "-45deg",
+      0: "0",
+      45: "45deg",
+      90: "90deg",
+      135: "135deg",
+      180: "180deg",
+      270: "270deg",
+    },
     extend: {
+      fontFamily: {
+        Header: ["Lato", "sans-serif"],
+        Text2: ["Lato", "sans-serif"],
+        About: ["Playfair Display", "serif"],
+        Arimo: ["Arimo", "sans-serif"],
+      },
       colors: {
+        primary: "#FFFFFF",
+        secondary: "#3D52A0",
+        tertiary: "#8697CA",
+        textmain: "#26283C",
+        sideicon: "#7091E6",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+        "3xl": "1920px",
+        "4xl": "2560px",
+        "5xl": "3840px",
+      },
     },
   },
-  plugins: [],
+  //plugins: []
+  plugins: [require("tailwind-scrollbar"), require("tailwind-scrollbar-hide")],
+  variants: {
+    scrollbar: ["rounded"],
+  }
 };
